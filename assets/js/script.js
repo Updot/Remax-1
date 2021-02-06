bfd5d51538f5ad29757c5efe07528d5f66cfaaf8
01abhishek10 <itsabhihere@yahoo.com>
2021-02-06 21:53:01 +0530

    added events

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,53 @@
+/*	gallery */
+$(document).ready(function(){
+    console.log('coming here');
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            $('.filter').show('1000');
+        }
+        else
+        {
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+
+	        	if ($(".filter-button").removeClass("active")) {
+			$(this).removeClass("active");
+		    }
+		    	$(this).addClass("active");
+	    	});
+});
+/*	end gallery */
+
+$(document).ready(function(){
+    $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+});
+
+$(document).ready(function () {
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("collapse show");
+        if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+            $("button.navbar-toggler").click();
+        }
+    });
+});
+
 function reduce(){
     var ele = document.getElementById('header-btn');
     if(screen.width <=568){
         ele.className="btn btn-primary btn-lg text-uppercase js-scroll-trigger";
         console.log('reduced');
     }
+}
+
+$(document).ready(function ()) {
+    
 }
