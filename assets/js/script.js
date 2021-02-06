e82aec33f74f56424549da7ac5cca3da12d54bb4
01abhishek10 <itsabhihere@yahoo.com>
2021-02-06 15:55:42 +0530

    added events

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,45 @@
+/*	gallery */
+$(document).ready(function(){
+    console.log('coming here');
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            $('.filter').show('1000');
+        }
+        else
+        {
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+
+	        	if ($(".filter-button").removeClass("active")) {
+			$(this).removeClass("active");
+		    }
+		    	$(this).addClass("active");
+	    	});
+});
+/*	end gallery */
+
+$(document).ready(function(){
+    $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+});
+
+$(document).ready(function () {
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("navbar-collapse in");
+        if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+            $("button.navbar-toggle").click();
+        }
+    });
+});
+
 function reduce(){
     var ele = document.getElementById('header-btn');
     if(screen.width <=568){
